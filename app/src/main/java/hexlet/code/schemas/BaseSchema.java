2cddccec9b1e09267116407b8e0bf3247cd537cd
Vlad Berezhnoy <vl.brzn@gmail.com>
2023-05-07 22:41:39 +0300

    minor fix

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,7 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public class BaseSchema {
+public abstract class BaseSchema {
     protected Map<String, Predicate> checks = new LinkedHashMap<>();
 
     protected final void addCheck(String name, Predicate validate) {
